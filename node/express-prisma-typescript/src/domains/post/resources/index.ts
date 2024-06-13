b8d67f0e07013be0cc7910ec651919420e04cf2d
Franco B. <franco.barravecchia2002@gmail.com>
2024-06-13 12:56:59 -0300

    update: post service integrated with AuthToSeeUserPosts

--- node/express-prisma-typescript/src/domains/post/resources/index.ts
+++ node/express-prisma-typescript/src/domains/post/resources/index.ts
@@ -1,5 +1,11 @@
 import db from "@utils/database";
 import { PostRepositoryImpl } from "../repository";
 import { PostService, PostServiceImpl } from "../service";
+import { UserIsPublicOrViewerFollowsUser } from "@domains/auth/service";
+import { UserRepositoryImpl } from "@domains/user/repository";
+import { FollowsUserImpl } from "@domains/follower";
 
-export const service: PostService = new PostServiceImpl(new PostRepositoryImpl(db))
+export const service: PostService = new PostServiceImpl(
+    new PostRepositoryImpl(db),
+    new UserIsPublicOrViewerFollowsUser(new UserRepositoryImpl(db), new FollowsUserImpl(db))
+)
