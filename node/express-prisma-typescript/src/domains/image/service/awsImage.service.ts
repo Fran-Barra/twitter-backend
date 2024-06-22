292dff9b51a4db95b8d4b659211a292f32c39214
Franco B. <franco.barravecchia2002@gmail.com>
2024-06-21 22:01:55 -0300

    feat: generateLinksForPostImages

--- node/express-prisma-typescript/src/domains/image/service/awsImage.service.ts
+++ node/express-prisma-typescript/src/domains/image/service/awsImage.service.ts
@@ -6,6 +6,7 @@ import { Constants } from "@utils/constants";
 export class AWSImageService implements ImageService {
     private readonly client;
     private readonly profilePictures = "profile-picture"
+    private readonly postImages = "post"
 
     constructor() {
         this.client = new S3Client({
@@ -32,5 +33,23 @@ export class AWSImageService implements ImageService {
             Key: `${this.profilePictures}/${userId}/profile.jpeg`
         }) 
         return await getSignedUrl(this.client, command)
+    }
+
+    generateLinksForPostImages(postId: string, amount: number) : Promise<string[]> {
+        const links = new Array(amount);
+        const promises = new Array(amount);
+
+        for (let i = 0; i<amount; i++) {
+            const key = `post/${postId}/picture${i}.jpeg`
+            const command = new PutObjectCommand({
+                Bucket: Constants.S3_BUCKET_NAME,
+                Key: key,
+                ContentType: "image/jpeg",
+                ACL: 'public-read'
+            })
+            promises[i] = this.client.send(command)
+            links[i] = key
+        }
+        return Promise.all(promises)
     }
 }
