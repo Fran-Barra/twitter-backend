de9a2a3aac50464b1e4d5c5e6ca2fdacd726b83e
Franco B. <franco.barravecchia2002@gmail.com>
2024-06-30 10:57:17 -0300

    faet: AuthToAddParticipantInChat

--- node/express-prisma-typescript/src/domains/auth/service/index.ts
+++ node/express-prisma-typescript/src/domains/auth/service/index.ts
@@ -2,3 +2,5 @@ export * from './auth.service'
 export * from './auth.service.impl'
 export * from "./authToSeeUserPosts.service"
 export * from './userIsPublicOrViewerFollowsUser.service.impl'
+export * from './authToAddParticipantInChat.service'
+export * from './usersFollowEachOtherAndChatParticipants.service.impl'
